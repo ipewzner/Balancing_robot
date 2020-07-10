1444b68106e3f28ff97d70a3311b74041e219a04
ipewzner <48483845+ipewzner@users.noreply.github.com>
2020-07-10 17:16:42 +0300

    test

---
+++ mpu_visualization.py
@@ -0,0 +1,33 @@
+import serial
+#import matplotlib
+import numpy
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+incuming = serial.Serial('com11', 115200) 
+print("------- start ---------")
+print(incuming)
+serial.
+print("-------- end ----------")
+
+"""
+print("------- start ---------")
+#plt.plot([1, 2, 3, 4], [1, 2, 3, 4])
+plt.plot([10, 5, 1, 4,3,0,11])
+plt.ylabel('some numbers')
+plt.grid(True)
+plt.show()
+print("-------- end ----------")
+
+plt.axis([0, 10, 0, 1])
+
+for i in range(100):
+    y = np.random.random()
+    plt.scatter(i, y)
+    plt.pause(0.05)
+print("----**----")
+
+plt.show()
+print("-------- end 2----------")
+"""
